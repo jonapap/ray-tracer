2ef78595d620d2377949e62785ff256666b65fa4
Jonathan Papineau <public@jpapineau.ca>
2021-11-03 20:09:02 -0400

    Add a simple sphere

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,22 @@ use crate::color::{write_color, Color};
 use crate::ray::Ray;
 use cgmath::{InnerSpace, Vector3};
 
+type Vec3 = Vector3<f64>;
+
+fn hit_sphere(center: Vec3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin - center;
+    let a = Vec3::dot(r.direction, r.direction);
+    let b = 2.0 * Vec3::dot(oc, r.direction);
+    let c = Vec3::dot(oc, oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction / r.direction.magnitude();
     let t = 0.5 * (unit_direction.y + 1.0);
 
@@ -16,7 +31,7 @@ fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
-    let image_height = (image_width as f64) / aspect_ratio;
+    let image_height = ((image_width as f64) / aspect_ratio) as i32;
 
     // Camera
 
@@ -29,9 +44,6 @@ fn main() {
     let vertical = Vector3::new(0.0, viewport_height, 0.0);
     let lower_left_corner =
         origin - horizontal / 2.0 - vertical / 2.0 - Vector3::new(0.0, 0.0, focal_length);
-
-    let image_width = 256;
-    let image_height = 256;
 
     println!("P3");
     println!("{} {}", image_width, image_height);
