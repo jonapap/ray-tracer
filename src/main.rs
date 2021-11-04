9869b4cf0b28411041c32f166b9291aeb4fe06ae
Jonathan Papineau <public@jpapineau.ca>
2021-11-04 17:24:20 -0400

    Add the hittable module

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,39 @@
 mod color;
+mod hittable;
 mod ray;
 mod vec3;
 
 use crate::color::{write_color, Color};
+use crate::hittable::*;
 use crate::ray::Ray;
-use crate::vec3::unit_direction;
+use crate::vec3::unit_vector;
 use crate::vec3::Vec3;
 use cgmath::{InnerSpace, Vector3};
 
-fn hit_sphere(center: Vec3, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin - center;
-    let a = r.direction.magnitude2();
-    let half_b = Vec3::dot(oc, r.direction);
-    let c = oc.magnitude2() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
+fn ray_color(r: &Ray, world: &Hittable) -> Color {
+    match world.hit(r, 0.0, f64::INFINITY) {
+        Some(rec) => 0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0)),
+        None => {
+            let unit_direction = unit_vector(&r.direction);
+            let t = 0.5 * (unit_direction.y + 1.0);
 
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-half_b - discriminant.sqrt()) / a
+            (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+        }
     }
-}
-
-fn ray_color(r: &Ray) -> Color {
-    let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r);
-    if t > 0.0 {
-        let n = unit_direction(&(r.at(t) - Vec3::new(0.0, 0.0, -1.0)));
-
-        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
-    }
-
-    let unit_direction = r.direction / r.direction.magnitude();
-    let t = 0.5 * (unit_direction.y + 1.0);
-
-    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
 fn main() {
     // Image
+
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = ((image_width as f64) / aspect_ratio) as i32;
+
+    // World
+
+    let mut world = HittableList::new();
+    world.add(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
+    world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
 
@@ -69,7 +62,7 @@ fn main() {
                 lower_left_corner + u * horizontal + v * vertical - origin,
             );
 
-            let pixel_color = ray_color(&r);
+            let pixel_color = ray_color(&r, &world);
 
             write_color(pixel_color);
         }
