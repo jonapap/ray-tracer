ee1634e886ddf87d7ae05914f79f25e520680a83
Jonathan Papineau <public@jpapineau.ca>
2021-11-05 00:57:31 -0400

    Add antialiasing

--- src/base.rs
+++ src/base.rs
@@ -1,18 +1,25 @@
+use cgmath::num_traits::clamp;
 use cgmath::{InnerSpace, Vector3};
 
 pub type Color = Vector3<f64>;
 
-pub(crate) type Vec3 = Vector3<f64>;
+pub type Vec3 = Vector3<f64>;
 
 pub fn unit_vector(vec: &Vec3) -> Vec3 {
     vec / vec.magnitude()
 }
 
-pub fn write_color(color: Color) {
+pub fn write_color(pixel_color: Color, samples_per_pixel: u32) {
+    let scale = 1.0 / samples_per_pixel as f64;
+
+    let r = pixel_color.x * scale;
+    let g = pixel_color.y * scale;
+    let b = pixel_color.z * scale;
+
     println!(
         "{} {} {}",
-        (255.999 * color.x) as i32,
-        (255.999 * color.y) as i32,
-        (255.999 * color.z) as i32
+        (256.0 * clamp(r, 0.0, 0.999)) as i32,
+        (256.0 * clamp(g, 0.0, 0.999)) as i32,
+        (256.0 * clamp(b, 0.0, 0.999)) as i32
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod base;
+mod camera;
 mod hit;
 mod ray;
 
 use crate::base::*;
+use crate::camera::Camera;
 use crate::hit::sphere::Sphere;
 use crate::hit::*;
 use crate::ray::Ray;
+use rand::Rng;
 
 fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
     match world.hit(r, 0.0, f64::INFINITY) {
@@ -25,6 +28,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
     let image_height = ((image_width as f64) / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     // World
 
@@ -34,34 +38,27 @@ fn main() {
 
     // Camera
 
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new();
 
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
 
+    let mut rng = rand::thread_rng();
+
     for j in (0..(image_height - 1)).rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..image_width {
-            let u = (i as f64) / ((image_width - 1) as f64);
-            let v = (j as f64) / ((image_height - 1) as f64);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..samples_per_pixel {
+                let ran = rng.gen::<f64>();
+                let u = ((i as f64) + rng.gen::<f64>()) / (image_width - 1) as f64;
+                let v = ((j as f64) + rng.gen::<f64>()) / (image_height - 1) as f64;
 
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-
-            let pixel_color = ray_color(&r, &world);
-
-            write_color(pixel_color);
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+            write_color(pixel_color, samples_per_pixel);
         }
     }
 
