cb76c8ac4e4ef107bbfe2b9e1ae4265024d7f3c9
Jonathan Papineau <public@jpapineau.ca>
2021-11-09 13:19:48 -0500

    Remove the unnecessary time for bounding boxes

--- src/main.rs
+++ src/main.rs
@@ -52,10 +52,10 @@ fn main() {
         .get_matches();
 
     // Image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let aspect_ratio = 3.0 / 2.0;
+    let image_width = 1200;
     let image_height = ((image_width as f64) / aspect_ratio) as i32;
-    let samples_per_pixel = 100;
+    let samples_per_pixel = 500;
     let max_depth = 50;
 
     let (cam, world, background) = random_scene1(aspect_ratio);
@@ -69,7 +69,7 @@ fn main() {
     bar.set_draw_delta((image_height * image_width / 1000) as u64);
 
     println!("Starting to build BVH tree...");
-    let world = BVHNode::new_from_hittable_list(world, 0.0, 0.0);
+    let world = BVHNode::new_from_hittable_list(world);
     println!("Done building the tree");
 
     println!("Starting to render...");
@@ -78,9 +78,8 @@ fn main() {
         .rev()
         .cartesian_product(0..image_width)
         .collect::<Vec<_>>()
-        // .into_par_iter()
-        // .progress_with(bar)
-        .into_iter()
+        .into_par_iter()
+        .progress_with(bar)
         .map(|(j, i)| {
             let mut rng = SmallRng::from_rng(rand::thread_rng()).unwrap();
 
