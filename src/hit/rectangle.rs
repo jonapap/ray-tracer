c2b2a3768f03db100dac82526b60edf4f75397ba
Jonathan Papineau <dev@jontech.app>
2022-01-04 12:05:03 -0500

    Add the XZ and YZ rectangles

--- src/hit/rectangle.rs
+++ src/hit/rectangle.rs
@@ -60,3 +60,115 @@ impl<M: Material> Hittable for XYRect<M> {
         ))
     }
 }
+
+// XZRect
+
+pub struct XZRect<M: Material> {
+    x0: f64,
+    x1: f64,
+    z0: f64,
+    z1: f64,
+    k: f64,
+    mp: M,
+}
+
+impl<M: Material> XZRect<M> {
+    pub fn new(x0: f64, x1: f64, z0: f64, z1: f64, k: f64, mp: M) -> Self {
+        XZRect {
+            x0,
+            x1,
+            z0,
+            z1,
+            k,
+            mp,
+        }
+    }
+}
+
+impl<M: Material> Hittable for XZRect<M> {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let t = (self.k - r.origin.y) / r.direction.y;
+        if t < t_min || t > t_max {
+            return None;
+        }
+
+        let x = r.origin.x + t * r.direction.x;
+        let z = r.origin.z + t * r.direction.z;
+        if x < self.x0 || x > self.x1 || z < self.z0 || z > self.z1 {
+            return None;
+        }
+
+        Some(HitRecord::new(
+            t,
+            (x - self.x0) / (self.x1 - self.x0),
+            (z - self.z0) / (self.z1 - self.z0),
+            r.at(t),
+            &self.mp,
+            &r,
+            &Vec3::new(0.0, 1.0, 0.0),
+        ))
+    }
+
+    fn bounding_box(&self) -> Option<AABB> {
+        Some(AABB::new(
+            Point3::new(self.x0, self.k - 0.001, self.z0),
+            Point3::new(self.x1, self.k + 0.001, self.z1),
+        ))
+    }
+}
+
+// YZRect
+
+pub struct YZRect<M: Material> {
+    y0: f64,
+    y1: f64,
+    z0: f64,
+    z1: f64,
+    k: f64,
+    mp: M,
+}
+
+impl<M: Material> YZRect<M> {
+    pub fn new(y0: f64, y1: f64, z0: f64, z1: f64, k: f64, mp: M) -> Self {
+        YZRect {
+            y0,
+            y1,
+            z0,
+            z1,
+            k,
+            mp,
+        }
+    }
+}
+
+impl<M: Material> Hittable for YZRect<M> {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
+        let t = (self.k - r.origin.x) / r.direction.x;
+        if t < t_min || t > t_max {
+            return None;
+        }
+
+        let y = r.origin.y + t * r.direction.y;
+        let z = r.origin.z + t * r.direction.z;
+        if y < self.y0 || y > self.y1 || z < self.z0 || z > self.z1 {
+            return None;
+        }
+
+        Some(HitRecord::new(
+            t,
+            (y - self.y0) / (self.y1 - self.y0),
+            (z - self.z0) / (self.z1 - self.z0),
+            r.at(t),
+            &self.mp,
+            &r,
+            &Vec3::new(1.0, 0.0, 0.0),
+        ))
+    }
+
+    fn bounding_box(&self) -> Option<AABB> {
+        Some(AABB::new(
+            Point3::new(self.k - 0.001, self.y0, self.z0),
+            Point3::new(self.k + 0.001, self.y1, self.z1),
+        ))
+    }
+}
